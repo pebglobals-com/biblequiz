acee2320d7bed9e2ea56339eb23b8ba28be84296
pebglobals-com <pebglobals@gmail.com>
2026-07-18 20:44:53 +0100

    Revert broken Pages Functions, revert tsconfig change, add simple ping test

--- functions/senior/sermons/[slug].ts
+++
@@ -1,7 +0,0 @@
-import { renderDynamicPage } from "../../../lib/render-dynamic-page";
-
-export async function onRequest({ request, env }: { request: Request; env: any }) {
-  const url = new URL(request.url);
-  const slug = url.pathname.replace(/^\/senior\/sermons\//, "").replace(/\/$/, "");
-  return renderDynamicPage(request, env, "/senior/sermons/placeholder/", slug);
-}
--- functions/senior/sermons/[slug]/study.ts
+++
@@ -1,7 +0,0 @@
-import { renderDynamicPage } from "../../../../lib/render-dynamic-page";
-
-export async function onRequest({ request, env }: { request: Request; env: any }) {
-  const url = new URL(request.url);
-  const slug = url.pathname.match(/\/senior\/sermons\/([^/]+)\/study/)?.[1] || "";
-  return renderDynamicPage(request, env, "/senior/sermons/placeholder/study/", slug);
-}
