be312ec7a09aab717ce98c29fb6f8e6728b3f8a6
pebglobals-com <pebglobals@gmail.com>
2026-07-18 21:16:07 +0100

    test: simple Pages Function at [slug] paths

--- functions/junior/sermons/[slug]/study.ts
+++ functions/junior/sermons/[slug]/study.ts
@@ -1,9 +1,3 @@
-export async function onRequest({ request, env }: { request: Request; env: any }) {
-  const match = new URL(request.url).pathname.match(/\/junior\/sermons\/([^/]+)\/study/);
-  if (!match) return new Response("Not found", { status: 404 });
-  const ph = await env.ASSETS.fetch(new Request(new URL("/junior/sermons/placeholder/study/", request.url)));
-  if (!ph.ok) return new Response("Not found", { status: 404 });
-  return new Response((await ph.text()).replaceAll('"placeholder"', `"${match[1]}"`), {
-    headers: { "content-type": "text/html; charset=utf-8" },
-  });
+export async function onRequest() {
+  return new Response("study-function-works", { status: 200 });
 }
